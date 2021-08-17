8a50563868844a75698bb97231accf4f3516bcb9
Bartolomeu <pbartmr@gmail.com>
2021-08-17 16:42:13 +0100

    Added integrity-check command + update script instructions improvement

--- update-project-boilerplate.sh
+++ update-project-boilerplate.sh
@@ -43,9 +43,11 @@ To continue with the update write yes and press Enter.
   - Confirm that new commits from the project boilerplate are in the project's git history by running 'git log'
     - If no new commits from the project boilerplate are present, you can try running the update script again
 
-- If there are git conflicts present, resolve the conflicts by pick the updates you want to add to the project
+- If there are git conflicts present, resolve the conflicts by picking the updates you want to add to the project
 
 - Run 'npm install', in case any dependencies were changed or added
+
+- Run 'npm run integrity-check' to make sure the updates didn't break any part of your project
 
 - Run 'git add .' to stage all the accepted updates
 
