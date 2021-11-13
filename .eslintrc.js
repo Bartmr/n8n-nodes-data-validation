8c9faf005ebc0abadfae82040c373ed533e41c95
Bartolomeu <pbartmr@gmail.com>
2021-11-13 13:23:52 +0000

    Added @typescript-eslint/no-unsafe-argument rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     "@typescript-eslint/no-unsafe-call": "error",
     "@typescript-eslint/no-unsafe-member-access": "error",
     "@typescript-eslint/no-unsafe-return": "error",
+    "@typescript-eslint/no-unsafe-argument": "error",
     "@typescript-eslint/restrict-template-expressions": [
       "error",
       { allowNumber: true, allowNullish: false },
